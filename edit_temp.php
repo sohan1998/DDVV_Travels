b7b8dad6336f4f67a001e1e606a23f7561c2a544
Sohan Shirodkar <sohan1998@github.com>
2018-10-19 09:03:50 +0530

    Changes to edit_temp.php

--- edit_temp.php
+++ edit_temp.php
@@ -25,6 +25,9 @@ $email=$_SESSION['email'];
 $name=$_SESSION['name'];
 $cust_id=$_SESSION['c_id'];
 
+if (!$name) {
+	header("Location: login.html");
+}
 
 //$message = "Username and/or Password incorrect.\\nTry again.";
 //echo "<script type='text/javascript'>alert('$message');</script>";
@@ -38,126 +41,126 @@ $cust_id=$_SESSION['c_id'];
 <html>
 <head>
   <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="styles.css">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="styles.css">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <title>My Account</title>
 </head>
 <body>
   <div class="fixed-top">
   <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-    <div class="container">
-      <a class="navbar-brand" href="index.php">DDVV Tours and Travels</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="oi oi-menu"></span> Menu
-      </button>
+	<div class="container">
+	  <a class="navbar-brand" href="index.php">DDVV Tours and Travels</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="oi oi-menu"></span> Menu
+	  </button>
 
-      <div class="collapse navbar-collapse" id="ftco-nav">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
-          <li class="nav-item"><a href="destinations.php" class="nav-link">Destinations</a></li>
-          <li class="nav-item"><a href="tours.php" class="nav-link">Tours</a></li>
-          <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" data-toggle="dropdown">
-            Account
-        </a>
-        <?php 
-          if (isset($_SESSION['c_id'])) {
-            echo '<div class="dropdown-menu">
-          <a class="dropdown-item" href="logout.php">Logout</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="my_account.php">Account info</a>
-          </div>';
-          }
-          else{
-            echo '<div class="dropdown-menu">
-        <a class="dropdown-item" href="login.html">Login</a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="sign_up.html">Sign Up</a>
-    </div>';
-          }
-      ?>
+	  <div class="collapse navbar-collapse" id="ftco-nav">
+		<ul class="navbar-nav ml-auto">
+		  <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+		  <li class="nav-item"><a href="destinations.php" class="nav-link">Destinations</a></li>
+		  <li class="nav-item"><a href="tours.php" class="nav-link">Tours</a></li>
+		  <li class="nav-item dropdown">
+		<a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" data-toggle="dropdown">
+			Account
+		</a>
+		<?php 
+		  if (isset($_SESSION['c_id'])) {
+			echo '<div class="dropdown-menu">
+		  <a class="dropdown-item" href="logout.php">Logout</a>
+		  <div class="dropdown-divider"></div>
+		  <a class="dropdown-item" href="my_account.php">Account info</a>
+		  </div>';
+		  }
+		  else{
+			echo '<div class="dropdown-menu">
+		<a class="dropdown-item" href="login.html">Login</a>
+		<div class="dropdown-divider"></div>
+		<a class="dropdown-item" href="sign_up.html">Sign Up</a>
+	</div>';
+		  }
+	  ?>
 </li>
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown">
-            Book Tours
-        </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="booking_default.php">DDVV Special Tour</a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="booking_custom.php">Customize Your Dream Tour</a>
-    </div>
+		<li class="nav-item dropdown">
+		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown">
+			Book Tours
+		</a>
+	  <div class="dropdown-menu">
+		<a class="dropdown-item" href="booking_default.php">DDVV Special Tour</a>
+		<div class="dropdown-divider"></div>
+		<a class="dropdown-item" href="booking_custom.php">Customize Your Dream Tour</a>
+	</div>
 </li>
-          <li class="nav-item"><a href="about_us.php" class="nav-link">About Us</a></li>
-        </ul>
-      </div>
-    </div>
+		  <li class="nav-item"><a href="about_us.php" class="nav-link">About Us</a></li>
+		</ul>
+	  </div>
+	</div>
   </nav>
 </div>
   
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-    <h1 class="display-3">Welcome to DDVV Tours and Travels!</h1>
+	<div class="jumbotron jumbotron-fluid">
+	  <div class="container">
+	<h1 class="display-3">Welcome to DDVV Tours and Travels!</h1>
 </div>
-    
+	
 </div>
 
 <div class="container">
   <div class="row mt-sm-4 mt-md-0">
-    <div class="col-sm-12 col-md-8">
-      <!-- <img src="images/sample.jpg" class="img-circle" style="border-radius: 50%" width="150px"><br><br> -->
-     <form action="edit.php" method="POST">
-       <div class="form-group">
-          <input type="text" class="form-control" name="name_edit" placeholder="Edit name" value="<?php echo $name; ?>">
-       </div>
-       <div class="form-group">
-          <input type="text" class="form-control" name="email_edit" placeholder="Edit email address" value="<?php echo $email; ?>">
-       </div>
-       <div class="form-group">
-          <input type="submit" value="Save changes" class="btn btn-primary py-2 px-4">
-       </div>
-     </form>
+	<div class="col-sm-12 col-md-8">
+	  <!-- <img src="images/sample.jpg" class="img-circle" style="border-radius: 50%" width="150px"><br><br> -->
+	 <form action="edit.php" method="POST">
+	   <div class="form-group">
+		  <input type="text" class="form-control" name="name_edit" placeholder="Edit name" value="<?php echo $name; ?>">
+	   </div>
+	   <div class="form-group">
+		  <input type="text" class="form-control" name="email_edit" placeholder="Edit email address" value="<?php echo $email; ?>">
+	   </div>
+	   <div class="form-group">
+		  <input type="submit" value="Save changes" class="btn btn-primary py-2 px-4">
+	   </div>
+	 </form>
 
-      <!-- <h3>Edit Name: <?php //echo $name; ?></h3>
-      <h3>User ID: <?php //echo $cust_id; ?></h3>
-      <h3> Edit Email ID: <?php //echo $email; ?></h3> -->
-    </div>
+	  <!-- <h3>Edit Name: <?php //echo $name; ?></h3>
+	  <h3>User ID: <?php //echo $cust_id; ?></h3>
+	  <h3> Edit Email ID: <?php //echo $email; ?></h3> -->
+	</div>
 
-    <div class="col-sm-12 col-md-4">
-        <h3 class="mb-4">Settings</h3>
+	<div class="col-sm-12 col-md-4">
+		<h3 class="mb-4">Settings</h3>
 
-        <ul class="nav flex-column nav-pills">
-            <li class="nav-item">
-                <a class="nav-link" name="previous_account" href="about_us.html">View Previous Tours</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" name="signout_account" href="logout.php">Sign Out</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" name="follow_account" href="https://www.facebook.com/DDVV-Travels-1911381365838357/" target="_blank">Follow us on Facebook!</a>
-            </li>
-        </ul>
-    </div>
+		<ul class="nav flex-column nav-pills">
+			<li class="nav-item">
+				<a class="nav-link" name="previous_account" href="about_us.html">View Previous Tours</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" name="signout_account" href="logout.php">Sign Out</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" name="follow_account" href="https://www.facebook.com/DDVV-Travels-1911381365838357/" target="_blank">Follow us on Facebook!</a>
+			</li>
+		</ul>
+	</div>
 </div>
 
    
 </div>
   <footer class="ftco-footer ftco-bg-dark ftco-section">
-    <section class="ftco-section bg-dark ftco-light "> 
-    <div class="footer-copyright text-center text_color-light py-4">
-      <p class="text-light bg-dark"><!-- <h5 class="ftco-heading-5 ftco-heading-light"> -->© 2016 Copyright:
-      <a href="index.php"> DDVVToursAndTravels.com</a></p>
-    </div>
-    <!-- Copyright -->
+	<section class="ftco-section bg-dark ftco-light "> 
+	<div class="footer-copyright text-center text_color-light py-4">
+	  <p class="text-light bg-dark"><!-- <h5 class="ftco-heading-5 ftco-heading-light"> -->© 2016 Copyright:
+	  <a href="index.php"> DDVVToursAndTravels.com</a></p>
+	</div>
+	<!-- Copyright -->
 </section>
   </footer>
   <!-- Footer -->
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
